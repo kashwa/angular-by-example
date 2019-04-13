7333f0fb98303733105ab3defa5c5093b81295c9
aabedkashwa <kashwa80@gmail.com>
2019-04-13 10:51:37 +0200

    ngStyle & PIPEs

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = true;
   currentClasses = {};
+  currentStyles = {};
 
   constructor() { this.image = 'http://lorempixel.com/600/600/people/3'; }
 
@@ -29,7 +30,9 @@ export class UsersComponent implements OnInit {
           city: 'cairo'
         },
         image: this.image,
-        isActive: true
+        isActive: true,
+        balance: 100,
+        registered: new Date('01/02/2019 00:30:00')
       },
       {
         firstname: 'alaa',
@@ -40,7 +43,9 @@ export class UsersComponent implements OnInit {
           state: 'menofia',
           city: 'usa'
         },
-        image: this.image
+        image: this.image,
+        balance: 200,
+        registered: new Date('15/07/2018 00:30:00')
       },
       {
         firstname: 'zahraa',
@@ -52,7 +57,9 @@ export class UsersComponent implements OnInit {
           city: 'paris'
         },
         image: this.image,
-        isActive: true
+        isActive: true,
+        balance: 300,
+        registered: new Date('01/02/2019 10:30:10')
       }
     ];
 
@@ -74,6 +81,7 @@ export class UsersComponent implements OnInit {
     // })
 
     this.setCurrentClasses();
+    this.setCurrentStyles();
   }
 
   addUser(user: User) {
@@ -84,6 +92,13 @@ export class UsersComponent implements OnInit {
     this.currentClasses = {
       'btn-success': this.enableAdd, // class success will be added if (this.enabled = true)
       'big-text': this.showExtended
+    }
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px'
     }
   }
 
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -11,5 +11,6 @@ export interface User {
   },
   image?: string,
   isActive?: boolean,
-
+  balance?: number,
+  registered?: any,
 }

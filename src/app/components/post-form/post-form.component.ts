0ea15ccd8d11efcf94085b9011eb0a02ca19d828
aabedkashwa <kashwa80@gmail.com>
2019-04-15 12:39:17 +0200

    event emitter & output

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,6 +1,6 @@
 import { Post } from './../../models/Post';
 import { PostService } from './../../services/post.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-form',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  post: Post;
+  @Output() newPost: EventEmitter<Post> = new EventEmitter();
 
   constructor(private postService: PostService) { }
 
@@ -18,8 +20,12 @@ export class PostFormComponent implements OnInit {
     if (!title || !body) {
       alert('please make sure of your data');
     } else {
-      this.postService.savePost({ title, body } as Post).subscribe(post => console.log(post));
+      this.postService.savePost({ title, body } as Post).subscribe(post => {
+        // emit an event and use that to push (unshift)
+        this.newPost.emit(post);
+      });
     }
+    // reset.
   }
 
 }
